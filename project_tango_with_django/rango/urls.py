65b855e56f722c19ea13cc43729c259d048d8b0e
Altynbek <altynbek@whatever.com>
2016-07-06 17:32:09 +0600

    Commit from work PC

---
+++ project_tango_with_django/rango/urls.py
@@ -0,0 +1,9 @@
+﻿# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+from rango import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^about/', views.about, name='about'),
+]
